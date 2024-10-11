5849f3a5363fe9c136963fd53701e7c0703f4209
Gustavo Henrique França <141081247+gustavoofranca@users.noreply.github.com>
2024-10-11 20:21:46 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { FaEye, FaGoogle, FaEyeSlash } from 'react-icons/fa'; 
+import './App.css'; // Arquivo CSS para estilização
+import  Login  from "./images/login-bg.png"
+import Logo from "./images/quiet-logo.png"
 
-function App() {
+
+const LoginPage = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login-container">
+      <div className="login-left">
+        <img src={Login} alt="Car Image" className="car-image" />
+      </div>
+
+      <div className="login-right">
+        <div className="login-box">
+          <div className="logo-container">
+            <img src={Logo} alt="Quiet Racing Club" className="logo" />
+          </div>
+          
+          <h2>Criar uma conta</h2>
+
+          <form>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="example@gmail.com" required />
+            </div>
+
+            <div className="input-group">
+              <label htmlFor="password">Senha</label>
+              <div className="password-container">
+                <input type="password" id="password" placeholder="Digite sua senha" required />
+                <FaEye className="icon-eye" />
+              </div>
+            </div>
+
+            <button type="submit" className="btn-primary">Criar Conta</button>
+
+            <button type="button" className="btn-google">
+              <FaGoogle className="icon-google" /> Continue com Google
+            </button>
+
+            <p className="login-footer">
+              Já tem uma conta? <a href="/login">Faça Login</a>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default LoginPage;
