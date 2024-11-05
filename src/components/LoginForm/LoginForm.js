26d19b4c7985fab27e5d133eadd077c98dfdfa2d
Felipe Wendler <felipewendlerccbeu@gmail.com>
2024-11-04 22:05:59 -0300

    att

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { FaEye } from 'react-icons/fa';
 import './loginform.css';
 import { FcGoogle } from "react-icons/fc";
-import { redirect, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 // Bibliotecas para criação e verificação de Usuário
 import { auth } from '../../firebaseConnection';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
+
 
 const LoginComp = () => {
 
@@ -32,21 +34,6 @@ const LoginComp = () => {
     return () => verificarLogin();
   }, []);
 
-  async function novoUsuario() {
-    await createUserWithEmailAndPassword(auth, email, senha)
-      .then(() => {
-        alert('Usuário cadastrado com sucesso!');
-        setEmail('');
-        setSenha('');
-      })
-      .catch((error) => {
-        if (error.code === 'auth/weak-password') {
-          alert('Senha muito fraca!');
-        } else if (error.code === 'auth/email-already-in-use') {
-          alert('Email já cadastrado!');
-        }
-      });
-  }
 
   async function logarUsuario() {
     await signInWithEmailAndPassword(auth, email, senha)
@@ -59,14 +46,14 @@ const LoginComp = () => {
         });
         setEmail('');
         setSenha('');
-        redirect('/home');
+        navigate('/home');
       })
       .catch(() => {
         alert('Erro ao fazer o login!');
       });
   }
 
-  
+
 
   const navigate = useNavigate();
 
@@ -74,38 +61,76 @@ const LoginComp = () => {
 
 
   return (
-    <div className="login-box">
-      <div className="logo-container">
-        <img src={require('../../images/quiet-logo.png')} alt="Quiet Racing Club" className="logo" />
+    <>
+      <div className="login-box">
+        <div className="logo-container">
+          <img src={require('../../images/quiet-logo.png')} alt="Quiet Racing Club" className="logo" />
+        </div>
+
+        <h2>Bem vindo de volta!</h2>
+
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Digite um email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <div className="password-container">
+              <input type="password" placeholder="Digite uma senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+              <FaEye className="icon-eye" />
+            </div>
+          </div>
+
+          <button type="submit" className="btn-login" onClick={logarUsuario}>Entrar</button>
+
+          <button type="button" className="btn-google">
+            <FcGoogle className="icon-google" /> Continue com Google
+          </button>
+
+          <p className="login-footer">
+            Ainda não tem uma conta? <a href="/register">Cadastre-se</a>
+          </p>
+        </form>
       </div>
 
-      <h2>Criar uma conta</h2>
-
-      <form>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" placeholder="Digite um email" value={email} onChange={(e) => setEmail(e.target.value)} />
+{/*======== REGISTER CONTAINER ==================================================*/}
+      <div className="register-box">
+        <div className="logo-container">
+          <img src={require('../../images/quiet-logo.png')} alt="Quiet Racing Club" className="logo" />
         </div>
 
-        <div className="input-group">
-          <label htmlFor="password">Senha</label>
-          <div className="password-container">
-            <input type="password" placeholder="Digite uma senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
-            <FaEye className="icon-eye" />
+        <h2>Novo por aqui?</h2>
+
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Digite um email" value={email} onChange={(e) => setEmail(e.target.value)} />
           </div>
-        </div>
 
-        <button type="submit" className="btn-login" onClick={logarUsuario}>Entrar</button>
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <div className="password-container">
+              <input type="password" placeholder="Digite uma senha" value={senha} onChange={(e) => setSenha(e.target.value)} />
+              <FaEye className="icon-eye" />
+            </div>
+          </div>
 
-        <button type="button" className="btn-google">
-          <FcGoogle className="icon-google" /> Continue com Google
-        </button>
+          <button type="submit" className="btn-login" onClick={logarUsuario}>Entrar</button>
 
-        <p className="login-footer">
-          Já tem uma conta? <a href="/login">Faça Login</a>
-        </p>
-      </form>
-    </div>
+          <button type="button" className="btn-google">
+            <FcGoogle className="icon-google" /> Continue com Google
+          </button>
+
+          <p className="login-footer">
+            Ainda não tem uma conta? <a href="/register">Cadastre-se</a>
+          </p>
+        </form>
+      </div>
+    </>
+
+
   );
 };
 
