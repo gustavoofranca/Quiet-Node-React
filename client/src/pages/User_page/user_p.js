a8c7a3d4c50c71ad748469fb1d0c6839dd662639
Felipe Wendler <felipewendlerccbeu@gmail.com>
2024-11-07 22:00:55 -0300

    user

---
+++ client/src/pages/User_page/user_p.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './user_p.css';
+import UserConfig from '../../components/userConfig/userConfig_comp';
+
+
+const UserPage = () => {
+    return (
+        <div>
+            <UserConfig />
+        </div>
+    );
+};
+
+export default UserPage;
