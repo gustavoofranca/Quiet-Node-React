61624089233d093a2e56f8663eac9deac6f8204b
Felipe Wendler <141081156+LipeWendler@users.noreply.github.com>
2024-11-18 19:59:45 -0300

    routes

--- client/src/router.js
+++ client/src/router.js
@@ -1,1 +1,18 @@
-//
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/login_page/login_p';
+import HomePage from './pages/Home/home_p';
+// import NotFound from './components/NotFound'; // exemplo de uma página 404
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/home" element={<HomePage />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default AppRouter;
