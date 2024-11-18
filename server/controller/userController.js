bbd02362f5bae015b454ae87f19492b049d6e1f2
Felipe Wendler <141081156+LipeWendler@users.noreply.github.com>
2024-11-18 20:37:41 -0300

    Server

---
+++ server/controller/userController.js
@@ -0,0 +1,75 @@
+import { firebaseapp } from "../firebaseConnection";
+
+import {
+    getFirestore, collection, doc, addDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+} from 'firebase/firestore';
+
+const db = getFirestore(firebaseapp);
+
+
+// ======= INÍCIO CRUD ===================================================================
+
+export const createUser = async (req, res, next) => {
+    try {
+        const data = req.body;
+        await addDoc(collection(db, 'users'), data);
+        res.status(200).send('Usuário criado com sucesso!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await getDocs(collection(db, 'users'));
+        res.status(200).send(users);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+// Coletar dados de um usuário específico
+export const getUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await getDoc(doc(db, 'users', id));
+        if (user.exists()) {
+            res.status(200).send(user.data());
+        } else {
+            res.status(404).send('Usuário não encontrado!');
+        }
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const updateUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data = req.body;
+        const user = doc(db, 'users', id);
+        await updateDoc(user, data);
+        res.status(200).send('Usuário atualizado com sucesso!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const deleteUser = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await deleteDoc(doc(db, 'users', id));
+        res.status(200).send('Usuário deletado com sucesso!');
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+// ======= FIM CRUD =================================================================
+
+
+
---
+++ server/routes/postRoute.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { createPost, getPosts, getPost, updatePost, deletePost } from '../controller/postController';
+
+const router = express.Router();
+
+router.post('/home', createPost);
+router.get('/home', getPosts);
+router.get('/home/:id', getPost);
+router.put('/home/:id', updatePost);
+router.delete('/home/:id', deletePost);
+
+export default router;
